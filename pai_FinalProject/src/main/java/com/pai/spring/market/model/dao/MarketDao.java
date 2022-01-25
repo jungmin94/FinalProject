08878f4205ee9f4c50edda09982dce78feb6bcd3
inmo yang <yim85005849@gmail.com>
2022-01-25 17:43:28 +0900

    마켓 서비스/다오 초안 생성 및 상품클래스 초안 생성 -im-

---
+++ pai_FinalProject/src/main/java/com/pai/spring/market/model/dao/MarketDao.java
@@ -0,0 +1,5 @@
+package com.pai.spring.market.model.dao;
+
+public interface MarketDao {
+
+}
---
+++ pai_FinalProject/src/main/java/com/pai/spring/market/model/service/MarketServiceImpl.java
@@ -0,0 +1,5 @@
+package com.pai.spring.market.model.service;
+
+public class MarketServiceImpl implements MarketService {
+
+}
