34719c8dd1d7a5138c772b588299c00cf7fefa6a
inmo yang <yim85005849@gmail.com>
2022-01-27 01:19:11 +0900

    마켓 메인 판매량,평점 top3 mapper 작성 전 -im-

--- pai_FinalProject/src/main/java/com/pai/spring/market/model/dao/MarketDaoImpl.java
+++ pai_FinalProject/src/main/java/com/pai/spring/market/model/dao/MarketDaoImpl.java
@@ -1,5 +1,25 @@
 package com.pai.spring.market.model.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.pai.spring.market.model.vo.Goods;
+
 public class MarketDaoImpl implements MarketDao {
 
+	@Override
+	public List<Goods> bestSell(SqlSessionTemplate session) {
+		
+		return session.selectList("market.bestSell");
+	}
+
+	@Override
+	public List<Goods> bestReview(SqlSessionTemplate session) {
+	
+		return session.selectList("market.bestReview");
+	}
+
+	
+	
 }
