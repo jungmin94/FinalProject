21bb8e319bb85713497d7668f0cfa220da6ecd6b
inmo yang <yim85005849@gmail.com>
2022-01-26 22:30:25 +0900

    im

--- pai_FinalProject/src/main/java/com/pai/spring/market/model/service/MarketServiceImpl.java
+++ pai_FinalProject/src/main/java/com/pai/spring/market/model/service/MarketServiceImpl.java
@@ -1,5 +1,23 @@
 package com.pai.spring.market.model.service;
 
+import java.util.List;
+
+import com.pai.spring.market.model.vo.Goods;
+
 public class MarketServiceImpl implements MarketService {
 
+	@Override
+	public List<Goods> bestSell() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Goods> bestReview() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
 }
