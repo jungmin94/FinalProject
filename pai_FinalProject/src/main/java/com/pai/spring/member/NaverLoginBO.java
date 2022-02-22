e3d96160c71933b8b6ec5adb1fa02ca318f7d99d
silve <silve@DESKTOP-LKGIT0N>
2022-02-22 20:37:42 +0900

    네이버로그인 서버주소로 변경 -sg-

--- pai_FinalProject/src/main/java/com/pai/spring/member/NaverLoginBO.java
+++ pai_FinalProject/src/main/java/com/pai/spring/member/NaverLoginBO.java
@@ -22,7 +22,7 @@ public class NaverLoginBO {
 	//state: 애플리케이션이 생성한 상태 토큰
 	private final static String CLIENT_ID = "zEmxreNfa587zfLCN5zB";
 	private final static String CLIENT_SECRET = "q7nblVo0UY";
-	private final static String REDIRECT_URI = "http://localhost:9090/spring/member/callback.do";
+	private final static String REDIRECT_URI = "https://61.78.121.242:9999/GDJ41_pai_final/member/callback.do";
 	private final static String SESSION_STATE = "oauth_state";
 	/* 프로필 조회 API URL */
 	private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
